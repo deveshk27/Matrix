806817381c6b009106eda5549d75cbfb7cccbc17
Devesh Kashyap <devesh.pe@gmail.com>
2025-07-29 23:52:37 +0530

    Added Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import "./App.css";
-import { Landing } from "./components/Landing";
+import { Signin } from "./components/Signin";
+import { useEffect } from "react";
+import { auth } from "./utils/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
 
 function App() {
-
+  useEffect(() => {
+    onAuthStateChanged(auth, function (user) {
+      if(user) {
+        console.log("This is the user : ", user)
+      } else {
+        console.log("No logged in user.")
+      }
+    })
+  },[])
   return (
     <>
       <div>
-        <Landing />
-      </div>
+        <Signin />
+      </div> 
     </>
   );
 }
